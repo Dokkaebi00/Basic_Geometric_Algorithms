4fb472af994db3a01bf6a043c0d2c36d4185b0e8
Dokkaebi00 <quang009182@gmail.com>
2020-11-22 03:05:38 +0700

    add closet_pair_code

--- Primitve operations/Primitve operations/Source.cpp
+++ Primitve operations/Primitve operations/Source.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
+#define infinity 9999999
 
 //cau truc du lieu cho 1 diem
 struct Point
@@ -24,7 +25,6 @@ int CWW(Point a, Point b, Point c)
 	{
 		return 0;
 	}
-	cout << val << endl;
 	return (val > 0) ? 1 : 2;
 }
 
@@ -77,30 +77,122 @@ bool doIntersect(Point p1, Point q1, Point p2, Point q2)
 	return false;
 }
 
+bool isInside(Point polygon[], int n, Point p)
+{ 
+	if (n < 3)  return false;
+
+	Point extreme = { infinity, p.y };
+ 
+	int count = 0, i = 0;
+	do
+	{
+		int next = (i + 1) % n;
+
+		if (doIntersect(polygon[i], polygon[next], p, extreme))
+		{
+			if (CWW(polygon[i], p, polygon[next]) == 0)
+				return onSegment(polygon[i], p, polygon[next]);
+
+			count++;
+		}
+		i = next;
+	} while (i != 0);
+
+	
+	return count & 1;
+}
+
+
+void Jarvis_convexHull(Point points[], int n)
+{
+	if (n < 3) return;
+
+	
+	vector<Point> hull;
+
+	int l = 0;
+	for (int i = 1; i < n; i++)
+		if (points[i].x < points[l].x)
+			l = i;
+
+	int p = l, q;
+	do
+	{
+		hull.push_back(points[p]);
+
+		q = (p + 1) % n;
+		for (int i = 0; i < n; i++)
+		{
+			if (CWW(points[p], points[i], points[q]) == 2)
+				q = i;
+		} 
+		p = q;
+
+	} while (p != l); 
+
+	// Print Result 
+	for (int i = 0; i < hull.size(); i++)
+		cout << "(" << hull[i].x << ", "
+		<< hull[i].y << ")\n";
+}
+
 void main()
 {
-	Point p1;
-	Point q1;
-	Point p2;
-	Point q2;
+	
+	/*
+	//demo CWW
 
-	p1.x = 1; p1.y = 1;
-	q1.x = 2; q1.y = 3;
-	p2.x = 3; p2.y = 2;
-	q2.x = 4; q2.y = 2;
+	*/
 
-	int test = CWW(q1, p2, p1);
+	/*
+	//demo line intersect
+	Point p1 = {1, 1}, q1 = {10, 1};
+	Point p2 = {1, 2}, q2 = {10, 2};
 
-	cout << "test: "<<test << endl;
+	doIntersect(p1, q1, p2, q2)? cout << "Yes\n": cout << "No\n";
 
-	bool b = doIntersect(p1, q1, p2, q2);
+	p1 = {10, 0}, q1 = {0, 10};
+	p2 = {0, 0}, q2 = {10, 10};
+	doIntersect(p1, q1, p2, q2)? cout << "Yes\n": cout << "No\n";
 
-	if (b == true)
-	{
-		cout << "co cat nhau" << endl;
-	}
-	else
-	{
-		cout << "ko cat nhau" << endl;
-	}
+	p1 = {-5, -5}, q1 = {0, 0};
+	p2 = {1, 1}, q2 = {10, 10};
+	doIntersect(p1, q1, p2, q2)? cout << "Yes\n": cout << "No\n";
+	*/
+
+	
+	//demo inside or out side
+	/*
+	Point polygon1[] = {{0, 0}, {10, 0}, {10, 10}, {0, 10}};
+	int n = sizeof(polygon1)/sizeof(polygon1[0]);
+	Point p = {20, 20};
+	isInside(polygon1, n, p)? cout << "Yes \n": cout << "No \n";
+
+	p = {5, 5};
+	isInside(polygon1, n, p)? cout << "Yes \n": cout << "No \n";
+
+	Point polygon2[] = {{0, 0}, {5, 5}, {5, 0}};
+	p = {3, 3};
+	n = sizeof(polygon2)/sizeof(polygon2[0]);
+	isInside(polygon2, n, p)? cout << "Yes \n": cout << "No \n";
+
+	p = {5, 1};
+	isInside(polygon2, n, p)? cout << "Yes \n": cout << "No \n";
+
+	p = {8, 1};
+	isInside(polygon2, n, p)? cout << "Yes \n": cout << "No \n";
+
+	Point polygon3[] =  {{0, 0}, {10, 0}, {10, 10}, {0, 10}};
+	p = {-1,10};
+	n = sizeof(polygon3)/sizeof(polygon3[0]);
+	isInside(polygon3, n, p)? cout << "Yes \n": cout << "No \n";
+	*/
+
+	
+	//demo convex hull - Jarvis
+	Point points[] = { {0, 3}, {2, 2}, {1, 1}, {2, 1},
+					  {3, 0}, {0, 0}, {3, 3} };
+	int n = sizeof(points) / sizeof(points[0]);
+	Jarvis_convexHull(points, n);
+	
 }
